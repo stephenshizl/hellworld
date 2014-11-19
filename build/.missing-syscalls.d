798b83e490964c208bbd550b4a9366be57a94928
Pillar <zuobaozhu@gmail.com>
2014-11-19 08:54:15 -0800

    add sprd_tools output

---
+++ build/.missing-syscalls.d
@@ -0,0 +1,11 @@
+-:  \
+ /home/pillar/SC7731/sc7731_project/sc7731_kernel/include/linux/kconfig.h \
+ include/generated/autoconf.h arch/arm/include/generated/asm/types.h \
+ /home/pillar/SC7731/sc7731_project/sc7731_kernel/include/uapi/asm-generic/types.h \
+ /home/pillar/SC7731/sc7731_project/sc7731_kernel/include/asm-generic/int-ll64.h \
+ /home/pillar/SC7731/sc7731_project/sc7731_kernel/include/uapi/asm-generic/int-ll64.h \
+ arch/arm/include/generated/asm/bitsperlong.h \
+ /home/pillar/SC7731/sc7731_project/sc7731_kernel/include/asm-generic/bitsperlong.h \
+ /home/pillar/SC7731/sc7731_project/sc7731_kernel/include/uapi/asm-generic/bitsperlong.h \
+ /home/pillar/SC7731/sc7731_project/sc7731_kernel/arch/arm/include/asm/unistd.h \
+ /home/pillar/SC7731/sc7731_project/sc7731_kernel/arch/arm/include/uapi/asm/unistd.h
---
+++ build/.vmlinux.cmd
@@ -0,0 +1,1 @@
+cmd_vmlinux := /bin/bash /home/pillar/SC7731/sc7731_project/sc7731_kernel/scripts/link-vmlinux.sh arm-linux-gnueabihf-ld -EL  -p --no-undefined -X --build-id
